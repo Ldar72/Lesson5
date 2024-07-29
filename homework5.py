1e31eb4d0640a7d796a09dc138e6f3d2d2e468e0
Ldar72 <l-dar@mail.ru>
2024-07-29 16:40:14 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -4,7 +4,7 @@ print(immutable_var)
 # print(immutable_var)
 # Если мы попытаемся изменить значение в immutable_var, у нас ничего не получится.
 # Пример выше выдаст ошибку 'TypeError: 'tuple' object does not support item assignment'.
-# Это произошло потому, что (immutable_var) относится к типу "кортеж" (tuple), в чем мы можем легко убедиться:
+# Это произошло потому, что immutable_var относится к типу "кортеж" (tuple), в чем мы можем легко убедиться:
 # print (type(immutable_var))
 mutable_list = [1, 2, False , 'Start']
 mutable_list = mutable_list [0:4] + ['Modified Final']
