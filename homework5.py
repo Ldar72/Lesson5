a10c4f712519f520b4e8a5e87d1a443d43c33e31
Ldar72 <l-dar@mail.ru>
2024-07-29 23:22:34 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,4 +1,4 @@
-immutable_var = 1 , 2 , False , 'Start'
+immutable_var = 1, 2, False, 'Start'
 print(immutable_var)
 # immutable_var[1] = 42
 # print(immutable_var)
@@ -6,6 +6,6 @@ print(immutable_var)
 # Пример выше выдаст ошибку 'TypeError: 'tuple' object does not support item assignment'.
 # Это произошло потому, что immutable_var относится к типу "кортеж" (tuple), в чем мы можем легко убедиться:
 # print (type(immutable_var))
-mutable_list = [1, 2, False , 'Start']
-mutable_list = mutable_list [0:4] + ['Modified Final']
+mutable_list = [1, 2, False, 'Start']
+mutable_list = mutable_list[0:4] + ['Modified Final']
 print(mutable_list)
